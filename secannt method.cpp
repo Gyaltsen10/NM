ed6779923c792e88370d194ee3cd7dbeb44c9a50
Gyaltsen10 <91243780+Gyaltsen10@users.noreply.github.com>
2022-08-24 15:47:43 +0545

    Add files via upload

---
+++ secannt method.cpp
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+
+
+#define    f(x)    x*x*x - 2*x - 5
+
+int main()
+{
+	 float x0, x1, x2, f0, f1, f2, e;
+	 int itr = 1, max;
+	 
+	 
+	 printf("\nEnter initial value:\n");
+	 scanf("%f%f", &x0, &x1);
+	 printf("Enter error:\n");
+	 scanf("%f", &e);
+	 printf("Enter maximum iteration:\n");
+	 scanf("%d", &max);
+	
+	 
+	 printf("\nitr\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
+	 do
+	 {
+		  f0 = f(x0);
+		  f1 = f(x1);
+		  if(f0 == f1)
+		  {
+			   printf("Mathematical Error.");
+			   exit(0);
+		  }
+		  
+		  x2 = x1 - (x1 - x0) * f1/(f1-f0);
+		  f2 = f(x2);
+		  
+		  printf("%d\t\t%f\t%f\t%f\t%f\n",itr,x0,x1,x2, f2);
+		  
+		  x0 = x1;
+		  f0 = f1;
+		  x1 = x2;
+		  f1 = f2;
+		  
+		  itr = itr + 1;
+		  
+		  if(itr > max)
+		  {
+			   printf("Not Convergent.");
+			   exit(0);
+		  }
+	 }while(fabs(f2)>e);
+	
+	 printf("\nRoot is: %f", x2);
+	 getch();
+}
+
